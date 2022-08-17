629cf3e2e196cf1ea566f1c2e47ea48e4ef841e4
programist <zhanybaevttt@gmail.com>
2022-08-17 18:10:23 +0300

    commit

--- src/City.java
+++ src/City.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class City {
+import java.util.Comparator;
+
+public class City {
+    private int code;
+    private String name;
+
+    public City(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return " City " +
+                " code " + code +
+                "  name  " + name +  '\'' +
+                ' ';
+    }
+    public static Comparator<City> cityComparator= new Comparator<City>() {
+        @Override
+        public int compare(City o1, City o2) {
+            return o2.code-o1.code;
+        }
+    };
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TreeSet<City> tashSet=new TreeSet<>(City.cityComparator);
+        tashSet.add(new City(6,"Tokyo"));
+        tashSet.add(new City(2,"London"));
+        tashSet.add(new City(7,"Nyu York"));
+        tashSet.add(new City(5,"Moscow"));
+        tashSet.add(new City(9,"Bishkek"));
+        tashSet.add(new City(8,"Osh"));
+
+
+
+
+
+        for (City d:tashSet) {
+            if (d.getCode()%2 == 1) {
+                System.out.println(d);
+            }
+
+        }
+
     }
 }
